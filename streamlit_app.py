2e1618d62ccd40ed4ad2d8cf88c4eccfce688e9c
MarouenMlouka <marouen.mlouka@gmail.com>
2024-07-13 17:58:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,32 +11,35 @@ def main():
     }
 
     # Choix du produit
-    choix_produit = st.selectbox("Choisissez un produit", list(produits.values()))
+    choix_produit = st.selectbox("Choisissez un produit", list(produits.values()), key="choix_produit")
     produit_nom = [key for key, value in produits.items() if value == choix_produit][0]
 
     # Saisie des ventes totales par mois avant l'implémentation (T0)
-    ventes_initiales = st.text_input(f"Entrez les ventes totales par mois de {choix_produit} avant l'implémentation du projet (T0) :")
+    ventes_initiales = st.text_input(f"Entrez les ventes totales par mois de {choix_produit} avant l'implémentation du projet (T0) :", key="ventes_initiales")
     try:
         ventes_initiales = float(ventes_initiales)
     except ValueError:
         st.error("Veuillez entrer un nombre valide.")
 
     # Saisie de la période de mesure
-    periode_mesure = st.number_input("Entrez la période de mesure (en mois) :", min_value=1, step=1)
+    periode_mesure = st.number_input("Entrez la période de mesure (en mois) :", min_value=1, step=1, key="periode_mesure")
 
     # Saisie des ventes mensuelles pour chaque mois de la période de mesure
     ventes_mensuelles = []
     for mois in range(1, periode_mesure + 1):
-        ventes_mois = st.number_input(f"Entrez les ventes totales pour le mois {mois} (en boîtes) :", min_value=0, step=1)
+        ventes_mois = st.number_input(f"Entrez les ventes totales pour le mois {mois} (en boîtes) :", min_value=0, step=1, key=f"ventes_mois_{mois}")
         ventes_mensuelles.append(ventes_mois)
 
     # Calcul des boîtes supplémentaires vendues
     if st.button("Calculer les boîtes supplémentaires vendues"):
-        total_ventes_apres_investissement = sum(ventes_mensuelles)
-        boites_supplementaires = total_ventes_apres_investissement - (ventes_initiales * periode_mesure)
-        st.subheader("Boîtes supplémentaires vendues =")
-        st.write(boites_supplementaires)
-        st.text("Formule : Boîtes supplémentaires vendues = Total des ventes après investissement - (Ventes initiales par mois * Période de mesure)")
+        if ventes_initiales is not None and all(ventes_mensuelles):
+            total_ventes_apres_investissement = sum(ventes_mensuelles)
+            boites_supplementaires = total_ventes_apres_investissement - (ventes_initiales * periode_mesure)
+            st.subheader("Boîtes supplémentaires vendues =")
+            st.write(boites_supplementaires)
+            st.text("Formule : Boîtes supplémentaires vendues = Total des ventes après investissement - (Ventes initiales par mois * Période de mesure)")
+        else:
+            st.warning("Veuillez remplir toutes les informations nécessaires avant de calculer.")
 
 if __name__ == "__main__":
     main()
